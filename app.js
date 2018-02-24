c52655b21cf2ddb036c4f36df7a5ae1f20ae213f
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-02-23 21:47:56 -0500

    second try -

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
 
+/*
 app.listen(5000, function() {
   console.log('Example app listening on port Hu3');
+});
+*/
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+    console.log("funciona------> get", __dirname);
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
