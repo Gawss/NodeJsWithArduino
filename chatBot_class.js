8f5b2b1f2ebd6645ad9abb0f8a8ca20ee895f28f
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-02-24 11:33:04 -0500

    backtomybot

--- chatBot_class.js
+++ chatBot_class.js
@@ -5,402 +5,216 @@ const config = require('./config.js');//Module that returns the tokens.
 
 class chatBot_ {
 
-    constructor() {
-        this.apiaiApp = apiai(config.apiAI_token);
-        this.sessionIds = new Map();
-    }
+	constructor() {
+		this.apiaiApp = apiai(config.apiAI_token);
+		this.sessionIds = new Map();
+	}
 
-    sendMessage(text, id) {
+	sendMessage(text, id) {
 
-        if (!this.sessionIds.has(id)) {
-            this.sessionIds.set(id, uuid.v4());
-        }
+		if (!this.sessionIds.has(id)) {
+			this.sessionIds.set(id, uuid.v4());
+		}
 
-        let apiai = this.apiaiApp.textRequest(text, {
-            sessionId: this.sessionIds.get(id)
-        });
+		let apiai = this.apiaiApp.textRequest(text, {
+			sessionId: this.sessionIds.get(id)
+		});
 
-        apiai.on('response', (response) => {
-            console.log("req_message----->", response.result.fulfillment.messages);
-            if (response.result.fulfillment.messages.length) {
-                console.log("req_message----->1");
-                this.callbackFacebook(id, { text: response.result.fulfillment.messages[0].speech });
-            } else {
-                console.log("req_message----->2");
-                this.callbackFacebook(id, response.result.fulfillment.messages);
-            }
+		apiai.on('response', (response) => {
+			console.log("req_message----->", response.result.fulfillment.messages);
+			if (response.result.fulfillment.messages.length) {
+				console.log("req_message----->1");
+				this.callbackFacebook(id, { text: response.result.fulfillment.messages[0].speech });
+			} else {
+				console.log("req_message----->2");
+				this.callbackFacebook(id, response.result.fulfillment.messages);
+			}
 
-        });
+		});
 
-        apiai.on('error', (error) => {
-            console.log(error);
-        });
+		apiai.on('error', (error) => {
+			console.log(error);
+		});
 
-        apiai.end();
+		apiai.end();
 
-    }
+	}
 
-    sendEvent(event, id) {
+	sendEvent(event, id) {
 
-        if (!this.sessionIds.has(id)) {
-            this.sessionIds.set(id, uuid.v4());
-        }
+		if (!this.sessionIds.has(id)) {
+			this.sessionIds.set(id, uuid.v4());
+		}
 
-        let ev = {
-            name: event
-        };
+		let ev = {
+			name: event
+		};
 
-        let apiai = this.apiaiApp.eventRequest(ev, {
-            sessionId: this.sessionIds.get(id) // use any arbitrary id
-        });
-
-
-        apiai.on('response', (response) => {
-            console.log("req_event----->", response.result.fulfillment.messages);
-            if (response.result.fulfillment.messages.length) {
-                console.log("req_event----->1");
-                this.callbackFacebook(id, { text: response.result.fulfillment.messages[0].speech });
-            } else {
-                console.log("req_event----->2");
-                this.callbackFacebook(id, response.result.fulfillment.messages);
-            }
-
-        });
-
-        apiai.on('error', (error) => {
-            console.log(error);
-        });
-
-        apiai.end();
-
-    }
-
-    cancelContext(sessionId){
-        request({
-            url: 'https://api.api.ai/v1/query?v=' + config.v,
-            method: 'POST',
-            headers: {
-                'Authorization': "Bearer " + config.apiAI_developer_token
-            },
-            json: true,
-            body: {
-                query: "cancelar",
-                sessionId: sessionId,
-                lang: 'es',
-                resetContexts: true,
-            }
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            } else {
-                console.log('response-------->', response.body);
-            }
-        });
-    }
-
-    callbackFacebook(id, menssage) {
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: {
-                recipient: { id: id },
-                message: menssage
-            }
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
-
-    }
-
-    callbackFacebookTransfer(id, message) {
-        let k;
-
-        this.sessionIds.forEach((value, key) => {
-            if (value === id) {
-                k = key;
-            }
-        });
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: {
-                recipient: { id: k },
-                message: message
-            }
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
-
-    }
-
-    callbackTransfer(text, id, accountFrom) {
-        let k;
-
-        this.sessionIds.forEach((value, key) => {
-            if (value === id) {
-                k = key;
-            }
-        });
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: {
-                recipient: { id: k },
-                message: {
-                    attachment: {
-                        type: "image",
-                        payload: {
-                            url: "https://chatbot-todo1.herokuapp.com/" + id + ".png"
-                        }
-                    }
-                }
-            }
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            } else {
-
-                request({
-                    url: 'https://graph.facebook.com/v2.6/me/messages',
-                    qs: { access_token: config.facebook_token },
-                    method: 'POST',
-                    json: {
-                        recipient: { id: k },
-                        message: {
-                            text: "Deseas realizar alguna otra operación?",
-                            quick_replies: [
-                                {
-                                    content_type: "text",
-                                    title: "Saldo de " + accountFrom,
-                                    payload: "saldo"
-                                },
-                                {
-                                    content_type: "text",
-                                    title: "Otra transferencia",
-                                    payload: "transferencia"
-                                },
-                                {
-                                    content_type: "text",
-                                    title: "¿Qué más puedo hacer?",
-                                    payload: "ayuda"
-                                }
-                            ]
-                        }
-                    }
-                }, (error, response) => {
-                    if (error) {
-                        console.log('Error sending message: ', error);
-                    } else if (response.body.error) {
-                        console.log('Error: ', response.body.error);
-
-                    }
-                });
-
-            }
-        });
-
-    }
-
-    userInfoRequest(userId) {
-        console.log('userInfoRequest-userId', userId);
-
-        return new Promise((resolve, reject) => {
-
-            request({
-                method: 'GET',
-                uri: "https://graph.facebook.com/v2.6/" + userId + "?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=" + config.facebook_token
-            }, function (error, response) {
-                if (error) {
-                    console.error('Error while userInfoRequest: ', error);
-                    reject(error);
-                } else {
-                    var obj = JSON.parse(response.body);
-                    resolve(obj);
-                }
-            });
-        });
-
-    }
-
-    setupGetStartedButton(res) {
-        var data = {
-            setting_type: "call_to_actions",
-            thread_state: "new_thread",
-            call_to_actions: [
-                {
-                    payload: "getStarted"
-                }
-            ]
-        };
-
-        // Start the request
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: data
-        },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    // Print out the response body
-                    res.send(body);
-
-                } else {
-                    // TODO: Handle errors
-                    res.send(body);
-                }
-            });
-    }
+		let apiai = this.apiaiApp.eventRequest(ev, {
+			sessionId: this.sessionIds.get(id) // use any arbitrary id
+		});
 
 
-    setupMenu(res) {
-        var data = {
-            persistent_menu: [
-                {
-                    locale: "default",
-                    composer_input_disabled: false,
-                    "call_to_actions": [
-                        {
-                            "title": "Ayuda del chat",
-                            "type": "postback",
-                            "payload": "ayuda"
-                        },
-                        {
-                            "title": "Información de seguridad",
-                            "type": "postback",
-                            "payload": "informacion"
-                        }
-                    ]
-                }
-            ]
-        };
+		apiai.on('response', (response) => {
+			console.log("req_event----->", response.result.fulfillment.messages);
+			if (response.result.fulfillment.messages.length) {
+				console.log("req_event----->1");
+				this.callbackFacebook(id, { text: response.result.fulfillment.messages[0].speech });
+			} else {
+				console.log("req_event----->2");
+				this.callbackFacebook(id, response.result.fulfillment.messages);
+			}
 
-        // Start the request
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: data
-        },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    // Print out the response body
-                    res.send(body);
+		});
 
-                } else {
-                    // TODO: Handle errors
-                    res.send(body);
-                }
-            });
-    }
+		apiai.on('error', (error) => {
+			console.log(error);
+		});
 
-    setupAcountLinkingUrl(res) {
-        var data = {
-            account_linking_url: "https://chatbot-todo1.herokuapp.com/confirmAuth"
-        };
+		apiai.end();
 
-        // Start the request
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
-            qs: { access_token: config.facebook_token },
-            method: 'POST',
-            json: data
-        },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send(body);
+	}
 
-            } else {
-                // TODO: Handle errors
-                res.send(body);
-            }
-        });
-    }
+	cancelContext(sessionId){
+		request({
+			url: 'https://api.api.ai/v1/query?v=' + config.v,
+			method: 'POST',
+			headers: {
+				'Authorization': "Bearer " + config.apiAI_developer_token
+			},
+			json: true,
+			body: {
+				query: "cancelar",
+				sessionId: sessionId,
+				lang: 'es',
+				resetContexts: true,
+			}
+		}, (error, response) => {
+			if (error) {
+				console.log('Error sending message: ', error);
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error);
+			} else {
+				console.log('response-------->', response.body);
+			}
+		});
+	}
 
-    findAccount(text, criterion) {
+	callbackFacebook(id, menssage) {
 
-        let cuenta = listAccounts.accounts.find((element) => {
-            return element.alias === text && element.propia === criterion
-        });
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: { access_token: config.facebook_token },
+			method: 'POST',
+			json: {
+				recipient: { id: id },
+				message: menssage
+			}
+		}, (error, response) => {
+			if (error) {
+				console.log('Error sending message: ', error);
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error);
+			}
+		});
 
-        if (typeof cuenta === 'undefined') {
-            cuenta = listAccounts.accounts.find((element) => {
-                return element.type === text && element.propia === criterion
-            });
-        }
+	}
 
-        if (typeof cuenta === 'undefined') {
-            cuenta = listAccounts.accounts.find((element) => {
-                return element.id === text && element.propia === criterion
-            });
-        }
+	userInfoRequest(userId) {
+		console.log('userInfoRequest-userId', userId);
 
-        return cuenta;
-    }
+		return new Promise((resolve, reject) => {
 
-    listAccount(criterion) {
+			request({
+				method: 'GET',
+				uri: "https://graph.facebook.com/v2.6/" + userId + "?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=" + config.facebook_token
+			}, function (error, response) {
+				if (error) {
+					console.error('Error while userInfoRequest: ', error);
+					reject(error);
+				} else {
+					var obj = JSON.parse(response.body);
+					resolve(obj);
+				}
+			});
+		});
 
-        let list = listAccounts.accounts.filter((element) => {
-            return element.propia === criterion
-        });
+	}
 
-        return list;
-    }
+	setupGetStartedButton(res) {
+		var data = {
+			setting_type: "call_to_actions",
+			thread_state: "new_thread",
+			call_to_actions: [
+				{
+					payload: "getStarted"
+				}
+			]
+		};
 
-    getAccount(txt, criterion) {
-        let list = listAccounts.accounts.filter((element) => {
-            return element.id === txt && element.propia === criterion;
-        });
+		// Start the request
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+			qs: { access_token: config.facebook_token },
+			method: 'POST',
+			json: data
+		},
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					// Print out the response body
+					res.send(body);
 
-        if (list.length === 0) {
-            list = listAccounts.accounts.filter((element) => {
-                return element.alias === txt && element.propia === criterion;
-            });
-        }
+				} else {
+					// TODO: Handle errors
+					res.send(body);
+				}
+			});
+	}
 
-        if (list.length === 0) {
-            list = listAccounts.accounts.filter((element) => {
-                return element.type === txt && element.propia === criterion;
-            });
-        }
 
-        return list;
-    }
+	setupMenu(res) {
+		var data = {
+			persistent_menu: [
+				{
+					locale: "default",
+					composer_input_disabled: false,
+					"call_to_actions": [
+						{
+							"title": "Ayuda del chat",
+							"type": "postback",
+							"payload": "ayuda"
+						},
+						{
+							"title": "Información de seguridad",
+							"type": "postback",
+							"payload": "informacion"
+						}
+					]
+				}
+			]
+		};
 
-    listAccountDetail(id) {
-        let listDetail = listAccounts.details.filter((element) => {
-            return element.id === id
-        });
+		// Start the request
+		request
+		({
+				url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+				qs: { access_token: config.facebook_token },
+				method: 'POST',
+				json: data
+			},
+			function (error, response, body) {
+				if (!error && response.statusCode == 200)
+				{
+					// Print out the response body
+					res.send(body);
 
-        return listDetail;
-    }
+				} else {
+					// TODO: Handle errors
+					res.send(body);
+				}
+		});
+	}
 
-    listUser(user, pass){
-        let listUser = listAccounts.users.find((element) => {
-            return user === element.username && pass === element.password;
-        });
-
-        return listUser;
-    }
 }
 
 module.exports = chatBot_
