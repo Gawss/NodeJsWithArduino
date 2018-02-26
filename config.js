ea5a67c894bc4e9c2c2055a5f2523356c940b57e
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-02-26 12:15:48 -0500

    fb token

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //Configuration data
 module.exports = {
-  facebook_token : "EAACiBqo8ZCOkBAF4MfcKItM9etUn9jJoM9IwLDtAcQJGZAEfGesqwTZAaEQZAjHTIwsMZBEu9ZBAclsmCjO68fNPGVVkMzmXIJZC25Mh7rS2ElKg6VLsM0xiLRwLOhg2QxPedU39roCBvc3P9rSdRF5eeHqu3BysCGC8Sj5rgaPZCnLq1LnpgQWJ",
+  facebook_token : "1f3d152e4175bc184966a775c0f74f76",
   apiAI_token    : "b304fb977519466f931e225ce1ce2154",
   apiAI_developer_token: "30662a3b466f435a890b1f690f7c83f4",
   v: "20180223"
