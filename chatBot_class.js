e8c71ff8b57bc78513d2c6018530df03ea21080d
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-02-25 21:56:05 -0500

    callbackFacebok try

--- chatBot_class.js
+++ chatBot_class.js
@@ -216,5 +216,22 @@ class chatBot_ {
 	}
 
 }
+function callbackFacebook(id, menssage) {
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: config.facebook_token },
+		method: 'POST',
+		json: {
+			recipient: { id: id },
+			message: menssage
+		}
+	}, (error, response) => {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+}
 
 module.exports = chatBot_
