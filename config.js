8a9b6cfc22e7bcd0e110d80147fb926647050f92
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-02-26 21:33:58 -0500

    asda

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //Configuration data
 module.exports = {
   facebook_token : "EAACiBqo8ZCOkBABG7Rwfmtuv7b1mm837QRQP1qAJeAKQ7PLxaaeVeqwuGrSxDljZBgGmkWtog0ujrKEzZAFMUHuSJQC8qzodPLZCtxk7uNqsJayIaU2fwxeyq5mRqlnOQVLIHOxi9MyMZAL4gPsOsMGyEqcmJRtSLPZCEocv06EClSAd2XsQ55",
-  apiAI_token    : "b304fb977519466f931e225ce1ce2154",
-  apiAI_developer_token: "30662a3b466f435a890b1f690f7c83f4",
+  apiAI_token    : "d6d19a35d60944ec83ac4b9d2d210c6c",
+  apiAI_developer_token: "d205911a60b744e88ef58e6078af9fc7",
   v: "20180223"
 }
 
