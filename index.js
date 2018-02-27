a1a08bab75cb9f5a8f97b9361e3f2a2d09732142
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-02-27 11:55:05 -0500

    statur lul

--- index.js
+++ index.js
@@ -280,7 +280,7 @@ app.post('/webhook', (req, res) => {
 			let webhook_event = entry.messaging[0];
 			console.log(webhook_event);
 		});
-		res.statur(200).send('EVENT_RECEIVED');
+		res.status(200).send('EVENT_RECEIVED');
 	}else{
 		res.sendStatus(404);
 	}
