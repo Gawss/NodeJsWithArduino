c90924155f499b8a6f601d32daa4b11a2aee1787
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-03-10 20:07:06 -0500

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //Configuration data
 module.exports = {
   facebook_token : "EAACiBqo8ZCOkBABG7Rwfmtuv7b1mm837QRQP1qAJeAKQ7PLxaaeVeqwuGrSxDljZBgGmkWtog0ujrKEzZAFMUHuSJQC8qzodPLZCtxk7uNqsJayIaU2fwxeyq5mRqlnOQVLIHOxi9MyMZAL4gPsOsMGyEqcmJRtSLPZCEocv06EClSAd2XsQ55",
-  apiAI_token    : "d6d19a35d60944ec83ac4b9d2d210c6c",
-  apiAI_developer_token: "d205911a60b744e88ef58e6078af9fc7",
+  apiAI_token    : "b304fb977519466f931e225ce1ce2154",
+  apiAI_developer_token: "30662a3b466f435a890b1f690f7c83f4",
   v: "20180223"
 }
 
@@ -20,6 +20,6 @@ module.exports.headers = {
         "contentType": "application/json"
       };
 
-module.exports.serverApiStore = 'https://demo01.todo1.com/t1-as-trx-web/rest/5.0';
+//module.exports.serverApiStore = 'https://demo01.todo1.com/t1-as-trx-web/rest/5.0';
 
 
