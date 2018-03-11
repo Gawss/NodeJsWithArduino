330753df0f89a8b395ea3f08661feecc9d9c1198
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-03-10 22:56:11 -0500

    console.log(command)

--- actions/turn_on.js
+++ actions/turn_on.js
@@ -6,7 +6,7 @@ exports.turn_on = function (res, req) {
 	//let quick_replies = [];
 	let text = ' ';
 	let command = ((typeof req.body.result.contexts === 'undefined' || req.body.result.contexts.length === 0) ? '' : req.body.result.contexts[0].parameters.cmd);
-	
+	console.log(command)
 	if(command === 'on'){
 		console.log('Command "on" received');
 		
